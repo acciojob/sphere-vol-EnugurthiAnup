346a43145d33604f4b9c0b9e4d1bfd7dbda82596
EnugurthiAnup <eanup.736@gmail.com>
2025-11-11 23:25:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+
 function volume_sphere() {
+let r = document.getElementById('radius').value
+let v = document.getElementById('volume')
+	r = parseFloat(r);
     //Write your code here
-  
+	let volume = (4 / 3) * Math.PI * Math.pow(r, 3);
+	
+	 v.value = volume.toFixed(4)
+  return false;
 } 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+window.onload = function () {
+	 document.getElementById('MyForm').onsubmit = volume_sphere;
+} 
